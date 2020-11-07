214e6bebc820be10d978d73b11de77a646624e72
dzagaduroska <s.dzagaduroska@gmail.com>
2020-11-07 22:56:25 +0100

    Add javadoc for JsonNodeUtils class

--- src/main/java/com/dzagaduroska/great_circle_distance/json/JsonNodeUtils.java
+++ src/main/java/com/dzagaduroska/great_circle_distance/json/JsonNodeUtils.java
@@ -5,14 +5,27 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 
+/**
+ * JsonNode Utility Class.
+ */
 public class JsonNodeUtils {
 
     private static final ObjectMapper objectMapper = new ObjectMapper();
 
+    /**
+     * Creates an empty array node.
+     * @return the array node
+     */
     public static ArrayNode createArrayNode() {
         return objectMapper.createArrayNode();
     }
 
+    /**
+     * Maps text to json node.
+     *
+     * @param text the text
+     * @return the node
+     */
     public static JsonNode mapTextToJsonNode(String text) {
         JsonNode node = null;
         try {
@@ -23,6 +36,12 @@ public class JsonNodeUtils {
         return node;
     }
 
+    /**
+     * Adds the node to the array node.
+     *
+     * @param arrayNode the array node
+     * @param node the node to add to the array node
+     */
     public static void addToArrayNode(ArrayNode arrayNode, JsonNode node) {
         arrayNode.add(node);
     }
